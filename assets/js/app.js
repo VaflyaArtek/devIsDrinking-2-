e5cd3747329423f82907eb70b8060870e0eba6d4
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2023-10-14 01:08:18 +0300

    tokenomics

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,6 +39,28 @@ document.querySelectorAll(".menu__item").forEach((n) =>
     })
 );
 
+// print text
+const typedTextSpan = document.querySelector('.typed-text');
+const cursorSpan = document.querySelector('.cursor');
+
+const textArray = ["Dev is Drinking - The Cryptocurrency Club"];
+const typingDelay = 100;
+const textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    typedTextSpan.textContent += textArray[textArrayIndex][charIndex];
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(type, 1500);
+});
+
+
 // console
 const log = (text) => {
     const styles = [
