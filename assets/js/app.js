2a6e1cedc472d1a1d4a6219b6d810872c7237e32
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2023-10-15 00:00:41 +0300

    fix bugs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,31 @@
+// music
+const playMusic = (volume) => {
+    const audio = document.getElementById("music");
+    audio.volume = volume;
+    audio.play();
+    hidePopup();
+};
+
+const openPageWithoutMusic = () => {
+    hidePopup();
+};
+
+const hidePopup = () => {
+    const popup = document.getElementById("popup");
+    popup.style.display = "none";
+};
+
+const yesButton = document.getElementById("yesButton");
+const noButton = document.getElementById("noButton");
+const volumeSlider = document.getElementById("volumeSlider");
+
+yesButton.addEventListener("click", () => {
+    const volume = volumeSlider.value / 100;
+    playMusic(volume);
+});
+
+noButton.addEventListener("click", openPageWithoutMusic);
+
 // hamburger
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__list-wrapper");
@@ -40,8 +68,8 @@ document.querySelectorAll(".menu__item").forEach((n) =>
 );
 
 // print text
-const typedTextSpan = document.querySelector('.typed-text');
-const cursorSpan = document.querySelector('.cursor');
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
 
 const textArray = ["Dev is Drinking - The Cryptocurrency Club"];
 const typingDelay = 100;
@@ -56,28 +84,29 @@ function type() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     setTimeout(type, 1500);
 });
-
 
 // console
 const log = (text) => {
     const styles = [
-        'font-weight: bold',
-        'font-size: 50px',
-        'color: red',
-        'text-shadow: 3px 3px 0 rgb(217,31,38), 6px 6px 0 rgb(226,91,14), 9px 9px 0 rgb(245,221,8), 12px 12px 0 rgb(5,148,68), 15px 15px 0 rgb(2,135,206), 18px 18px 0 rgb(4,77,145), 21px 21px 0 rgb(42,21,113)'
+        "font-weight: bold",
+        "font-size: 50px",
+        "color: red",
+        "text-shadow: 3px 3px 0 rgb(217,31,38), 6px 6px 0 rgb(226,91,14), 9px 9px 0 rgb(245,221,8), 12px 12px 0 rgb(5,148,68), 15px 15px 0 rgb(2,135,206), 18px 18px 0 rgb(4,77,145), 21px 21px 0 rgb(42,21,113)",
     ];
 
-    console.log(`%c ${text}`, styles.join(';'));
+    console.log(`%c ${text}`, styles.join(";"));
 };
 
 for (let i = 0; i < 100; i++) {
-    log('Please help me');
-    log('Call 911!!');
-    log('Strangers locked me in a room and forced me to drink alcohol');
-    console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+    log("Please help me");
+    log("Call 911!!");
+    log("Strangers locked me in a room and forced me to drink alcohol");
+    console.log(
+        "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+    );
 }
 
 // tokenomics
