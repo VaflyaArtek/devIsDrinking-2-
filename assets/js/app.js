554d97c402d0485cf3130e554a2bc8dbbe1d80db
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2023-10-14 20:21:26 +0300

    before fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,15 +49,15 @@ const textArrayIndex = 0;
 let charIndex = 0;
 
 function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    typedTextSpan.textContent += textArray[textArrayIndex][charIndex];
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex][charIndex];
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(type, 1500);
+    setTimeout(type, 1500);
 });
 
 
@@ -79,3 +79,54 @@ for (let i = 0; i < 100; i++) {
     log('Strangers locked me in a room and forced me to drink alcohol');
     console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
 }
+
+// tokenomics
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programing-stats .details ul");
+
+const chartData = {
+    labels: [
+        "Team",
+        "Exchange Listing",
+        "Strategic Partners & Advisors",
+        "Staking",
+        "Company reserves",
+        "Marketing",
+        "Private Round",
+        "Hero Exlusive Presale",
+        "TGLP",
+    ],
+    data: [10, 20, 12, 20, 15, 3, 5, 10, 5],
+};
+
+new Chart(myChart, {
+    type: "doughnut",
+    data: {
+        labels: chartData.labels,
+        datasets: [
+            {
+                data: chartData.data,
+            },
+        ],
+    },
+    options: {
+        borderWidth: 8,
+        borderRadius: 2,
+        hoverBorderWidth: 0,
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+    },
+});
+
+const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+        let li = document.createElement("li");
+        li.innerHTML = `${l}: <span class="percentage">${chartData.data[i]}%</span>`;
+        ul.appendChild(li);
+    });
+};
+
+populateUl();
